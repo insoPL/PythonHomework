80bc45a876dccbb0f59218a9564229e8de1a0cbd
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-12-14 00:40:10 +0100

    10.8 init

---
+++ python10/10.8.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+class RandomQueue:
+
+    def __init__(self): pass
+
+    def insert(self, item): pass
+
+    def remove(self): pass   # zwraca losowy element
+
+    def is_empty(self): pass
+
+    def is_full(self): pass
