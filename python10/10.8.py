4e3af3841f3c26856d6f17e6b6245434f7ef3ed6
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-12-14 16:27:43 +0100

    10.8 release

--- python10/10.8.py
+++ python10/10.8.py
@@ -1,14 +1,36 @@
 # -*- coding: utf-8 -*-
+import random
 
 
 class RandomQueue:
 
-    def __init__(self): pass
+    def __init__(self):
+            self.items = []
 
-    def insert(self, item): pass
+    def insert(self, item):
+        self.items.append(item)
 
-    def remove(self): pass   # zwraca losowy element
+    def remove(self):   # zwraca losowy element
+        rand = random.randint(0, len(self)-1)
+        return self.items.pop(rand)
 
-    def is_empty(self): pass
+    def is_empty(self):
+        return not self.items
 
-    def is_full(self): pass
+    def __len__(self):
+        return len(self.items)
+
+    def is_full(self):             # nigdy nie jest pusta
+        return False
+
+    def __str__(self):             # podglądanie kolejki
+        return str(self.items)
+
+
+randomQueue = RandomQueue()
+randomQueue.insert(3)
+randomQueue.insert(7)
+randomQueue.insert(1)
+randomQueue.insert(4)
+
+print randomQueue.remove()
