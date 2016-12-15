b8aa814b7429ab610f09a805da426a44a004071a
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-12-15 20:42:40 +0100

    11.1 release

---
+++ python11/11.1.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import random
+
+
+def random_list(n):
+    re_list = range(n)
+    random.shuffle(re_list)
+    return re_list
+
+
+def swap(L, left, right):
+    """Zamiana miejscami dwóch elementów."""
+    # L[left], L[right] = L[right], L[left]
+    item = L[left]
+    L[left] = L[right]
+    L[right] = item
+
+
+def not_as_random_list(n):
+    re_list = range(n)
+    for i in xrange(n-1):
+        if random.randint(0, 1):
+            swap(re_list, i, i+1)
+    return re_list
+
+
+def not_as_random_list_revert(n):
+    re_list = range(n)
+    re_list.sort(reverse=True)
+    for i in xrange(n-1):
+        if random.randint(0, 1):
+            swap(re_list, i, i+1)
+    return re_list
+
+
+def gauss_random_number(maximum, odchylenie=2):
+    srednia = 0
+    podzial = 0
+    for i in range(odchylenie):
+        srednia += random.randint(0, maximum)
+        podzial += 1
+    return srednia/podzial
+
+
+def gauss_random_list(n):
+    re_list = list()
+    for i in xrange(n):
+        re_list.append(gauss_random_number(10))
+
+
+def random_list_from_given_list(n, k):
+    """N liczb w kolejności losowej, o wartościach powtarzających się, należących do zbioru k elementowego (k < N, np. k*k = N)."""
+    ret_list = list()
+    K = range(k)
+    for i in xrange(n):
+        ret_list.append(random.choice(K))
+    return ret_list
