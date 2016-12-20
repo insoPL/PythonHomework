524305df91538d4448f311c5e943be1edaf85d41
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-12-20 02:29:06 +0100

    13.4 release

--- python13/13.4.py
+++ python13/13.4.py
@@ -2,6 +2,7 @@
 
 
 def print_sudoku(plansza):
+    global suma
     # +-----+-----+
     print ("+"+5*"-")*2+"+"
     print "| " + str(plansza[3, 0]) + " " + str(plansza[3, 1]) + " | " + str(plansza[3, 2]) + " " + str(plansza[3, 3]) + " |"
@@ -10,6 +11,9 @@ def print_sudoku(plansza):
     print "| " + str(plansza[1, 0]) + " " + str(plansza[1, 1]) + " | " + str(plansza[1, 2]) + " " + str(plansza[1, 3]) + " |"
     print "| " + str(plansza[0, 0]) + " " + str(plansza[0, 1]) + " | " + str(plansza[0, 2]) + " " + str(plansza[0, 3]) + " |"
     print ("+" + 5 * "-") * 2 + "+"
+    print suma
+    print
+    suma += 1
 
 
 def where_empty(plansza):
@@ -28,7 +32,7 @@ def empty_plansza():
     return plansza
 
 
-def if_good(plansza, xx, yy, value):
+def if_good(plansza, value, xx, yy):
     for y in range(yy):  # pionowo
         # (xx,y)
         if plansza[xx, y] == value:
@@ -38,14 +42,29 @@ def if_good(plansza, xx, yy, value):
         # (x, yy)
         if plansza[x, yy] == value:
             return False
+
+    # w kwadraciku
+    xk = xx/2
+    yk = yy/2  # yk = codry malego kwadraciku
+    for x in xrange(2):
+        for y in xrange(2):
+            if plansza[(xk*2)+x, (yk*2)+y] == value:
+                return False
     return True
 
 
-def sudoku(plansza):
+def sudoku(plansza, deep):
     cords = where_empty(plansza)
-    if cords == -1:
+    deep -= 1
+    if cords == -1 or deep == -1:
         print_sudoku(plansza)
         return
+    else:
+        x, y = cords
+        for i in xrange(1, 5):
+            if if_good(plansza, i, *cords):
+                plansza[cords] = i
+                sudoku(plansza.copy(), deep)
 
-
-sudoku(empty_plansza())
+suma = 1
+sudoku(empty_plansza(), 16)
