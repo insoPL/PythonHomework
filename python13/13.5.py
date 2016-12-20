bd78a35f53acd7668abc80bf61d302dca039a656
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-12-20 12:30:30 +0100

    13.5 release

--- python13/13.5.py
+++ python13/13.5.py
@@ -3,22 +3,26 @@
 
 def print_sudoku(plansza):
     global suma
+    suma += 1
+    if suma % 1000 != 0: #  wypisz co tysieczną plansze
+        return None
     # +-----+-----+
-    print ("+"+5*"-")*2+"+"
-    print "| " + str(plansza[0, 3]) + " " + str(plansza[1, 3]) + " | " + str(plansza[2, 3]) + " " + str(plansza[3, 3]) + " |"
-    print "| " + str(plansza[0, 2]) + " " + str(plansza[1, 2]) + " | " + str(plansza[2, 2]) + " " + str(plansza[3, 2]) + " |"
-    print ("+" + 5 * "-") * 2 + "+"
-    print "| " + str(plansza[0, 1]) + " " + str(plansza[1, 1]) + " | " + str(plansza[2, 1]) + " " + str(plansza[3, 1]) + " |"
-    print "| " + str(plansza[0, 0]) + " " + str(plansza[1, 0]) + " | " + str(plansza[2, 0]) + " " + str(plansza[3, 0]) + " |"
-    print ("+" + 5 * "-") * 2 + "+"
+    print ("+" + 5 * "-") * 3 + "+"
+    print "| " + str(plansza[0, 5]) + " " + str(plansza[1, 5]) + " | " + str(plansza[2, 5]) + " " + str(plansza[3, 5]) + " | " + str(plansza[4, 5]) + " " + str(plansza[5, 5]) + " |"
+    print "| " + str(plansza[0, 4]) + " " + str(plansza[1, 4]) + " | " + str(plansza[2, 4]) + " " + str(plansza[3, 4]) + " | " + str(plansza[4, 4]) + " " + str(plansza[5, 4]) + " |"
+    print "| " + str(plansza[0, 3]) + " " + str(plansza[1, 3]) + " | " + str(plansza[2, 3]) + " " + str(plansza[3, 3]) + " | " + str(plansza[4, 3]) + " " + str(plansza[5, 3]) + " |"
+    print ("+" + 5 * "-") * 3+ "+"
+    print "| " + str(plansza[0, 2]) + " " + str(plansza[1, 2]) + " | " + str(plansza[2, 2]) + " " + str(plansza[3, 2]) + " | " + str(plansza[4, 2]) + " " + str(plansza[5, 2]) + " |"
+    print "| " + str(plansza[0, 1]) + " " + str(plansza[1, 1]) + " | " + str(plansza[2, 1]) + " " + str(plansza[3, 1]) + " | " + str(plansza[4, 1]) + " " + str(plansza[5, 1]) + " |"
+    print "| " + str(plansza[0, 0]) + " " + str(plansza[1, 0]) + " | " + str(plansza[2, 0]) + " " + str(plansza[3, 0]) + " | " + str(plansza[4, 0]) + " " + str(plansza[5, 0]) + " |"
+    print ("+" + 5 * "-") * 3 + "+"
     print suma
     print
-    suma += 1
 
 
 def where_empty(plansza):
-    for x in xrange(4):
-        for y in xrange(4):
+    for x in xrange(6):
+        for y in xrange(6):
             if plansza[(x, y)] == 0:
                 return x, y
     return -1
@@ -26,8 +30,8 @@ def where_empty(plansza):
 
 def empty_plansza():
     plansza = dict()
-    for x in xrange(4):
-        for y in xrange(4):
+    for x in xrange(6):
+        for y in xrange(6):
             plansza[(x, y)] = 0
     return plansza
 
@@ -45,26 +49,26 @@ def if_good(plansza, value, xx, yy):
 
     # w kwadraciku
     xk = xx/2
-    yk = yy/2  # yk = codry malego kwadraciku
+    yk = yy/3  # yk = codry malego kwadraciku
     for x in xrange(2):
-        for y in xrange(2):
-            if plansza[(xk*2)+x, (yk*2)+y] == value:
+        for y in xrange(3):
+            if plansza[(xk*2)+x, (yk*3)+y] == value:
                 return False
+
     return True
 
 
-def sudoku(plansza, deep):
+def sudoku(plansza):
     cords = where_empty(plansza)
-    deep -= 1
-    if cords == -1 or deep == -1:
+    if cords == -1:
         print_sudoku(plansza)
         return
     else:
         x, y = cords
-        for i in xrange(1, 5):
+        for i in xrange(1, 7):
             if if_good(plansza, i, *cords):
                 plansza[cords] = i
-                sudoku(plansza.copy(), deep)
+                sudoku(plansza.copy())
 
-suma = 1
-sudoku(empty_plansza(), 16)
+suma = 0
+sudoku(empty_plansza())
