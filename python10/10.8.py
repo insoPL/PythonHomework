41f2559ae129fa62093c59b70ce2d3597a39f919
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-12-31 08:01:58 +0100

    python10 poprawiono randomQueue tak zeby dzialal w stalym czasie

--- python10/10.8.py
+++ python10/10.8.py
@@ -5,14 +5,18 @@ import random
 class RandomQueue:
 
     def __init__(self):
-            self.items = []
+        self.items = list()
 
     def insert(self, item):
         self.items.append(item)
 
     def remove(self):   # zwraca losowy element
-        rand = random.randint(0, len(self)-1)
-        return self.items.pop()
+        rand = random.randint(0, len(self.items)-1)
+        if rand == len(self.items)-1:  # jesli wylosowany element jest ostatni
+            return self.items.pop()
+        ret = self.items[rand]
+        self.items[rand] = self.items.pop()
+        return ret
 
     def is_empty(self):
         return not self.items
@@ -33,4 +37,10 @@ randomQueue.insert(7)
 randomQueue.insert(1)
 randomQueue.insert(4)
 
+print randomQueue
 print randomQueue.remove()
+print randomQueue
+print randomQueue.remove()
+print randomQueue
+print randomQueue.remove()
+print randomQueue
